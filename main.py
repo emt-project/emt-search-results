c342d757cc4608640ef93e37c38ccfefd0370c8e
csae8092 <peter.andorfer@oeaw.ac.at>
2023-05-03 14:41:47 +0200

    pagination should work #1

--- main.py
+++ main.py
@@ -19,21 +19,33 @@ with open("search_terms.txt") as f:
     search_terms = [line.rstrip() for line in f if not line.startswith("#")]
 print(search_terms)
 
-
 for search_term in search_terms:
-    result = client.collections['emt'].documents.search({
-        'q': search_term,
-        'query_by': 'full_text',
-        "per_page": 250
-    })
+    page = 1
+    per_page = 250
     data = []
-    for x in result["hits"]:
-        item = {
-            "id": x["document"]["id"],
-            "match": x["highlight"]["full_text"]["snippet"]
-        }
-        data.append(item)
-    out_file = os.path.join("out", f"{search_term}.csv")
-    df = pd.DataFrame(data)
-    df.to_csv(out_file, index=False)
+    more = True
+    while more:
+        result = client.collections['emt'].documents.search({
+            'q': search_term,
+            'query_by': 'full_text',
+            "per_page": per_page,
+            "page": page
+        })
+        print(result["found"] / per_page * page)
+        if (result["found"] / (per_page * page)) > 1:
+            more = True
+            page += 1
+        else:
+            more = False
+        print(page, more)
+        for x in result["hits"]:
+            item = {
+                "id": x["document"]["id"],
+                "match": x["highlight"]["full_text"]["snippet"]
+            }
+            data.append(item)
+    
+        out_file = os.path.join("out", f"{search_term}.csv")
+        df = pd.DataFrame(data)
+        df.to_csv(out_file, index=False)
 print("done")
